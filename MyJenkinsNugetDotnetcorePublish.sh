b23d59ca759a2f5fff06bc64219f520106ea1707
Calos <7870328+CalosChen@users.noreply.github.com>
2019-01-15 18:53:39 +0800

    Update MyJenkinsNugetDotnetcorePublish.sh

--- MyJenkinsNugetDotnetcorePublish.sh
+++ MyJenkinsNugetDotnetcorePublish.sh
@@ -1,5 +1,5 @@
 
-
+dotnet restore
 dotnet pack -c Release
 
 if ${usenuget}
@@ -8,7 +8,8 @@ then
 	cd ${WORKSPACE}/EP.${fragment}/bin/Release
 	echo $pwd
         dotnet nuget delete --source http://192.168.1.7:17071/ -k 3cdb9803-86f9-4750-9615-338f14f0c814 --non-interactive EP.${fragment} 1.0.0
-	dotnet nuget push -Source http://192.168.1.7:17071/ -ApiKey 3cdb9803-86f9-4750-9615-338f14f0c814 EP.${fragment}.1.0.0.nupkg
-        done
+        #ls | grep *.nupkg
+	dotnet nuget push --source http://192.168.1.7:17071/ -k 3cdb9803-86f9-4750-9615-338f14f0c814 EP.${fragment}.1.0.0.nupkg
+    done
 fi
 	   
