2341aef6140b49e291b0d31283d233615c0fb9d0
Calos <7870328+CalosChen@users.noreply.github.com>
2019-01-15 16:10:30 +0800

    Delete Bash.sh

--- Bash.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-echo "yes"
-#no space allowed between variable name and =, "true" "false" is defined this way
-usenuget="true"
-#use brace to wrap defined variable
-if ${usenuget}
-then
-	echo "publish"
-fi
