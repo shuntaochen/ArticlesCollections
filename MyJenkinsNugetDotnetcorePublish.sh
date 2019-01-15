cf831576db07958d6989292b62041bc0d9e5b77c
Calos <7870328+CalosChen@users.noreply.github.com>
2019-01-15 16:15:58 +0800

    Update MyJenkinsNugetDotnetcorePublish.sh

--- MyJenkinsNugetDotnetcorePublish.sh
+++ MyJenkinsNugetDotnetcorePublish.sh
@@ -5,7 +5,7 @@ then
 	for fragment in Commons.Core Commons.Config Commons.Migrator Commons.Permission Commons.Rest RemoteEventBus RemoteEventBus.Redis; do
 	cd ${WORKSPACE}/EP.${fragment}/obj/Release
 	echo $pwd
-        dotnet nuget delete -Source http://192.168.1.7:17071/ -ApiKey 3cdb9803-86f9-4750-9615-338f14f0c814 EP.${fragment}.1.0.0.nupkg
-	dotnet nuget push -Source http://192.168.1.7:17071/ -ApiKey 3cdb9803-86f9-4750-9615-338f14f0c814 EP.${fragment}.1.0.0.nupkg
+        dotnet nuget delete --source http://192.168.1.7:17071/ -ApiKey 3cdb9803-86f9-4750-9615-338f14f0c814 EP.${fragment}.1.0.0.nupkg
+	dotnet nuget push --source http://192.168.1.7:17071/ -ApiKey 3cdb9803-86f9-4750-9615-338f14f0c814 EP.${fragment}.1.0.0.nupkg
 done
 fi
