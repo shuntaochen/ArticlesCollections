87aba559b0d31adbf68c7024e8ac034911718242
Calos <7870328+CalosChen@users.noreply.github.com>
2019-01-18 15:00:49 +0800

    Update MyJenkinsNugetDotnetcorePublish.sh

--- MyJenkinsNugetDotnetcorePublish.sh
+++ MyJenkinsNugetDotnetcorePublish.sh
@@ -2,15 +2,21 @@
 dotnet restore
 dotnet pack -c Release
 
+
+
+
+
 if ${usenuget}
 then
-	for fragment in Commons.Core Commons.Config Commons.Migrator Commons.Permission Commons.Rest RemoteEventBus RemoteEventBus.Redis; do
+	for fragment in EP.Commons.Core EP.Commons.Config EP.Commons.Migrator EP.Commons.Permission EP.Commons.Rest EP.RemoteEventBus EP.RemoteEventBus.Redis NanoFabric.Core NanoFabric.RegistryHost.ConsulRegistry NanoFabric.Router; do
 	cd ${WORKSPACE}/EP.${fragment}/bin/Release
 	echo $pwd
-        dotnet nuget delete --source http://192.168.1.7:17071/ -k 3cdb9803-86f9-4750-9615-338f14f0c814 --non-interactive EP.${fragment} 1.0.0
-        #ls | grep *.nupkg
-	#find ./ -type f -name \*.nupkg| while read line; do echo $line; done 
-	dotnet nuget push --source http://192.168.1.7:17071/ -k 3cdb9803-86f9-4750-9615-338f14f0c814 EP.${fragment}.1.0.0.nupkg
+    dotnet nuget delete --source http://192.168.1.7:17071/ -k 3cdb9803-86f9-4750-9615-338f14f0c814 --non-interactive ${fragment} 1.0.0
     done
+    cd ${WORKSPACE}
+    find ./ -type f -name \*.nupkg| while read line; do 
+	echo $line;
+	dotnet nuget push --source http://192.168.1.7:17071/ -k 3cdb9803-86f9-4750-9615-338f14f0c814 $line
+	done
 fi
 	   
