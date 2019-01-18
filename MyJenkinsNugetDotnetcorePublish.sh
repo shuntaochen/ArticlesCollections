f954bab7bc1bf2e60a3c416f84b7e674b3abbd7b
Calos <7870328+CalosChen@users.noreply.github.com>
2019-01-18 14:41:28 +0800

    Update MyJenkinsNugetDotnetcorePublish.sh

--- MyJenkinsNugetDotnetcorePublish.sh
+++ MyJenkinsNugetDotnetcorePublish.sh
@@ -9,6 +9,7 @@ then
 	echo $pwd
         dotnet nuget delete --source http://192.168.1.7:17071/ -k 3cdb9803-86f9-4750-9615-338f14f0c814 --non-interactive EP.${fragment} 1.0.0
         #ls | grep *.nupkg
+	#find ./ -type f -name \*.nupkg| while read line; do echo $line; done 
 	dotnet nuget push --source http://192.168.1.7:17071/ -k 3cdb9803-86f9-4750-9615-338f14f0c814 EP.${fragment}.1.0.0.nupkg
     done
 fi
