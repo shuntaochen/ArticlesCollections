26486a5c087edb97359d361b0b9ce4bf8338d8ba
Calos <7870328+CalosChen@users.noreply.github.com>
2019-01-23 16:02:18 +0800

    Update MyJenkinsNugetDotnetcorePublish.sh

--- MyJenkinsNugetDotnetcorePublish.sh
+++ MyJenkinsNugetDotnetcorePublish.sh
@@ -1,13 +1,10 @@
-
+find ./ -type f -name \*.nupkg -delete
 dotnet restore
 dotnet pack -c Release
 
 
 if ${usenuget}
 then
-	#for fragment in EP.Commons.Core EP.Commons.Config EP.Commons.Migrator EP.Commons.Permission EP.Commons.Rest EP.RemoteEventBus EP.RemoteEventBus.Redis NanoFabric.Core NanoFabric.RegistryHost.ConsulRegistry NanoFabric.Router; do
-    #dotnet nuget delete --source http://192.168.1.7:17071/ -k 3cdb9803-86f9-4750-9615-338f14f0c814 --non-interactive ${fragment} 1.0.0
-    #done
     cd ${WORKSPACE}
     find ./ -type f -name \*1.0.0.nupkg| while read line; do 
 	echo $line;
