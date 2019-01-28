410c4370b6f814d388d62cb45ba850027ca5b2f1
Calos <7870328+CalosChen@users.noreply.github.com>
2019-01-28 09:59:44 +0800

    Create MyDockerPlainBuildWithParameter.sh

---
+++ MyDockerPlainBuildWithParameter.sh
@@ -0,0 +1,23 @@
+dispose(){
+cd ${WORKSPACE}
+docker stop ep-ocelot
+docker rm ep-ocelot
+docker rmi epocelotgateway
+}
+launch(){
+cd ${WORKSPACE}
+docker build -t epocelotgateway .
+docker run -d --name ep-ocelot -p 18030:18030 epocelotgateway
+
+}
+if test ${ISINIT}=="TRUE"
+then
+launch
+else
+dispose
+launch
+fi
+
+
+
+
