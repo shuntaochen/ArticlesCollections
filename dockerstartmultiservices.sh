264fee9d47aa5123283cea5ce58b0843368ea02c
Calos <7870328+CalosChen@users.noreply.github.com>
2019-02-11 11:51:52 +0800

    Update dockerstartmultiservices.sh

--- dockerstartmultiservices.sh
+++ dockerstartmultiservices.sh
@@ -1,4 +1,4 @@
-#put this to some location, by default, echo outputs a new line, if want to append to current line, add -n
+#put this to some location, by default, echo outputs a new line, if want to append to current line, add -n,some linux versions need no white space
 echo "service1 service2 ..." >> a.txt
 echo -n " service99" >> a.txt
 docker start `cat a.txt`
