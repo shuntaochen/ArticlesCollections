3a0915e37bd6fbad2a61a13facac8c96a418f545
Calos <7870328+CalosChen@users.noreply.github.com>
2019-02-18 13:38:23 +0800

    Update MyJenkinsNugetDotnetcorePublish.sh

--- MyJenkinsNugetDotnetcorePublish.sh
+++ MyJenkinsNugetDotnetcorePublish.sh
@@ -2,15 +2,28 @@ find ./ -type f -name \*.nupkg -delete
 dotnet restore
 dotnet pack -c Release
 
-
-if ${usenuget}
-then
+dispose(){
     cd ${WORKSPACE}
     find ./ -type f -name \*1.0.0.nupkg| while read line; do 
 	echo $line;
     right=${line%%.1.0.0.nupkg}
     dotnet nuget delete --source http://192.168.1.7:17071/ -k 3cdb9803-86f9-4750-9615-338f14f0c814 --non-interactive ${right##*/} 1.0.0
+	done
+}
+push(){
+    cd ${WORKSPACE}
+    find ./ -type f -name \*1.0.0.nupkg| while read line; do 
+	echo $line;
+    right=${line%%.1.0.0.nupkg}
 	dotnet nuget push --source http://192.168.1.7:17071/ -k 3cdb9803-86f9-4750-9615-338f14f0c814 $line
 	done
+    }
+
+if test ${emptyinventory} = True
+then
+dispose
 fi
-	   
+if test ${pushtoinventoty} = True
+then
+push
+fi
