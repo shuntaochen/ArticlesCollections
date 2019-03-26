248b3f9163fb47cb716c048ccb95af1796e3391b
Calos <7870328+CalosChen@users.noreply.github.com>
2019-03-26 14:16:24 +0800

    Update CaloChHttpBriefClient.cs

--- CaloChHttpBriefClient.cs
+++ CaloChHttpBriefClient.cs
@@ -176,3 +176,17 @@ https://docs.microsoft.com/en-us/dotnet/api/system.dynamic.dynamicobject.tryconv
         {
             Config = option.Value;
         }
+//
+            //Post the embeded json files to usercenter language text database;
+            var nms = typeof(ServiceTemplateCoreModule).Assembly.GetManifestResourceNames();
+            var translationSrc = typeof(ServiceTemplateCoreModule).Assembly.GetManifestResourceStream("EP.ServiceTemplate.Localize.SourceFiles.ServiceTemplate-zh-Hans.json");
+            var sr = new StreamReader(translationSrc);
+            using (sr)
+            {
+                var str = sr.ReadToEnd();
+                this.SafelyRun(() =>
+                {
+                    //Post the language texts to usercenter service.
+                    AsyncHelper.RunSync(() => UserCenterRestContext.Instance.PostAsync("language/Upload", str.ToJsonStringContent()));
+                });
+            }
