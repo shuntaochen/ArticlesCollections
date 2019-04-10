e80894d30186468474b6cd342282f34267b587c7
Calos <7870328+CalosChen@users.noreply.github.com>
2019-04-10 10:54:18 +0800

    Update ConcurrentAmbientDataProvider.cs

--- CaloChSimpleComponents/ConcurrentAmbientDataProvider.cs
+++ CaloChSimpleComponents/ConcurrentAmbientDataProvider.cs
@@ -414,3 +414,26 @@ namespace EP.DynamicForms.Helpers
 
 
 //data context is a concurrent dictionary
+//# using System.Collections.Concurrent;
+using System.Threading;
+using Abp.Dependency;
+
+namespace Abp.Runtime.Remoting
+{
+    public class AsyncLocalAmbientDataContext : IAmbientDataContext, ISingletonDependency
+    {
+        private static readonly ConcurrentDictionary<string, AsyncLocal<object>> AsyncLocalDictionary = new ConcurrentDictionary<string, AsyncLocal<object>>();
+
+        public void SetData(string key, object value)
+        {
+            var asyncLocal = AsyncLocalDictionary.GetOrAdd(key, (k) => new AsyncLocal<object>());
+            asyncLocal.Value = value;
+        }
+
+        public object GetData(string key)
+        {
+            var asyncLocal = AsyncLocalDictionary.GetOrAdd(key, (k) => new AsyncLocal<object>());
+            return asyncLocal.Value;
+        }
+    }
+}
