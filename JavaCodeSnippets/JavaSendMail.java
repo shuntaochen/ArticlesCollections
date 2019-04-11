59eb0346aab57d055e437d4d9423df1cb435c27a
Calos <7870328+CalosChen@users.noreply.github.com>
2019-04-11 08:26:13 +0800

    Update JavaSendMail.java

--- JavaCodeSnippets/JavaSendMail.java
+++ JavaCodeSnippets/JavaSendMail.java
@@ -52,3 +52,162 @@ public class SendEmail
       }
    }
 }
+
+// SendMail with attachments 带附件下载
+
+// 文件名 SendFileEmail.java
+ 
+import java.util.*;
+import javax.mail.*;
+import javax.mail.internet.*;
+import javax.activation.*;
+ 
+public class SendFileEmail
+{
+   public static void main(String [] args)
+   {
+     
+      // 收件人电子邮箱
+      String to = "abcd@gmail.com";
+ 
+      // 发件人电子邮箱
+      String from = "web@gmail.com";
+ 
+      // 指定发送邮件的主机为 localhost
+      String host = "localhost";
+ 
+      // 获取系统属性
+      Properties properties = System.getProperties();
+ 
+      // 设置邮件服务器
+      properties.setProperty("mail.smtp.host", host);
+ 
+      // 获取默认的 Session 对象。
+      Session session = Session.getDefaultInstance(properties);
+ 
+      try{
+         // 创建默认的 MimeMessage 对象。
+         MimeMessage message = new MimeMessage(session);
+ 
+         // Set From: 头部头字段
+         message.setFrom(new InternetAddress(from));
+ 
+         // Set To: 头部头字段
+         message.addRecipient(Message.RecipientType.TO,
+                                  new InternetAddress(to));
+ 
+         // Set Subject: 头字段
+         message.setSubject("This is the Subject Line!");
+ 
+         // 创建消息部分
+         BodyPart messageBodyPart = new MimeBodyPart();
+ 
+         // 消息
+         messageBodyPart.setText("This is message body");
+        
+         // 创建多重消息
+         Multipart multipart = new MimeMultipart();
+ 
+         // 设置文本消息部分
+         multipart.addBodyPart(messageBodyPart);
+ 
+         // 附件部分
+         messageBodyPart = new MimeBodyPart();
+         String filename = "file.txt";
+         DataSource source = new FileDataSource(filename);
+         messageBodyPart.setDataHandler(new DataHandler(source));
+         messageBodyPart.setFileName(filename);
+         multipart.addBodyPart(messageBodyPart);
+ 
+         // 发送完整消息
+         message.setContent(multipart );
+ 
+         //   发送消息
+         Transport.send(message);
+         System.out.println("Sent message successfully....");
+      }catch (MessagingException mex) {
+         mex.printStackTrace();
+      }
+   }
+}
+
+//用户认证
+props.put("mail.smtp.auth", "true");
+props.setProperty("mail.user", "myuser");
+props.setProperty("mail.password", "mypwd");
+//SSl
+// 关于QQ邮箱，还要设置SSL加密，加上以下代码即可
+MailSSLSocketFactory sf = new MailSSLSocketFactory();
+sf.setTrustAllHosts(true);
+props.put("mail.smtp.ssl.enable", "true");
+props.put("mail.smtp.ssl.socketFactory", sf);
+
+//qq邮箱
+import java.security.GeneralSecurityException;
+import java.util.Properties;
+import javax.mail.Authenticator;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import com.sun.mail.util.MailSSLSocketFactory;
+
+public class SendEmail 
+{
+    public static void main(String [] args) throws GeneralSecurityException 
+    {
+        // 收件人电子邮箱
+        String to = "XXXXX@qq.com";
+
+        // 发件人电子邮箱
+        String from = "XXXXXX@qq.com";
+
+        // 指定发送邮件的主机为 smtp.qq.com
+        String host = "smtp.qq.com";  //QQ 邮件服务器
+
+        // 获取系统属性
+        Properties properties = System.getProperties();
+
+        // 设置邮件服务器
+        properties.setProperty("mail.smtp.host", host);
+
+        properties.put("mail.smtp.auth", "true");
+        MailSSLSocketFactory sf = new MailSSLSocketFactory();
+        sf.setTrustAllHosts(true);
+        properties.put("mail.smtp.ssl.enable", "true");
+        properties.put("mail.smtp.ssl.socketFactory", sf);
+        // 获取默认session对象
+        Session session = Session.getDefaultInstance(properties,new Authenticator(){
+            public PasswordAuthentication getPasswordAuthentication()
+            {
+                return new PasswordAuthentication("429240967@qq.com", "授权的 QQ 邮箱密码"); //发件人邮件用户名、密码
+            }
+        });
+
+        try{
+            // 创建默认的 MimeMessage 对象
+            MimeMessage message = new MimeMessage(session);
+
+            // Set From: 头部头字段
+            message.setFrom(new InternetAddress(from));
+
+            // Set To: 头部头字段
+            message.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
+
+            // Set Subject: 头部头字段
+            message.setSubject("This is the Subject Line!");
+
+            // 设置消息体
+            message.setText("This is actual message");
+
+            // 发送消息
+            Transport.send(message);
+            System.out.println("Sent message successfully....from runoob.com");
+        }catch (MessagingException mex) {
+            mex.printStackTrace();
+        }
+    }
+}
